a8725252cdbd33cb85d5044b2355392288481a1b
fromsi-arch <fromsi665@gmail.com>
2021-03-14 23:09:59 +0600

    feat: add gitlab calendar

--- README.md
+++ README.md
@@ -3,4 +3,4 @@
 1) Склонировать репозиторий
 2) Загрузить зависимости `go mod download`
 3) Запустить `go run main.go`
-4) Зайти http://localhost:2004
+4) Зайти http://localhost:2004/gitlab/{username}
--- main.go
+++ main.go
@@ -1,17 +1,22 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"github.com/ajstarks/svgo"
+	"io"
 	"log"
-	"math/rand"
 	"net/http"
+	"time"
 )
 
+// Структура квадратов
 type SquareCalendar struct {
 	size   int
 	margin int
 }
 
+// Структура поле для квадратов
 type Calendar struct {
 	days           int
 	margin         int
@@ -21,7 +26,7 @@ type Calendar struct {
 
 // Запус слушателя
 func main() {
-	http.Handle("/", http.HandlerFunc(render))
+	http.HandleFunc("/gitlab/", handlerRoute)
 
 	err := http.ListenAndServe(":2004", nil)
 
@@ -31,48 +36,121 @@ func main() {
 }
 
 // Генерация SVG в виде конечного календаря
-func render(w http.ResponseWriter, req *http.Request) {
+func handlerRoute(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "image/svg+xml")
 
-	calendar := Calendar{days: 7, margin: 20}
-	calendar.squareCalendar = SquareCalendar{size: 15, margin: 2}
-	calendar.data = chunkBy(make([]int, 336), calendar.days) //TODO: пока в разработке
+	//убрать "/gitlab/" символы
+	trimmedURL := r.URL.Path[8:]
 
-	s := svg.New(w)
-	s.Start(
-		len(calendar.data)*calendar.squareCalendar.size+calendar.margin,
-		calendar.days*calendar.squareCalendar.size+calendar.margin,
-	)
+	if trimmedURL != "" {
+		var title string
 
-	renderCalendar(s, &calendar)
+		if string(trimmedURL[len(trimmedURL)-1]) == "/" {
+			title = trimmedURL[0:(len(trimmedURL) - 1)]
+		} else {
+			title = trimmedURL
+		}
 
-	s.End()
+		calendar := Calendar{days: 7, margin: 20}
+		calendar.squareCalendar = SquareCalendar{size: 15, margin: 2}
+		calendar.data = getCalendarCommitDataGitlab(title)
+
+		s := svg.New(w)
+		s.Start(
+			len(calendar.data)*calendar.squareCalendar.size+calendar.margin,
+			calendar.days*calendar.squareCalendar.size+calendar.margin,
+		)
+
+		renderCalendar(s, &calendar)
+
+		s.End()
+	} else {
+		handleErrorCode(404, "Path not found.", w)
+	}
 }
 
+// Генерация ошибочной страницы
+func handleErrorCode(errorCode int, description string, w http.ResponseWriter) {
+	w.WriteHeader(errorCode) // изменить HTTP status code (пример 404, 500)
+	w.Header().Set("Content-Type", "text/html")
+	_, _ = w.Write([]byte(fmt.Sprintf(
+		"<html><body><h1>Error %d</h1><p>%s</p></body></html>",
+		errorCode,
+		description)))
+}
+
+// Получить готовые данные в двумерном массиве с количеством коммитов
+func getCalendarCommitDataGitlab(username string) [][]int {
+	response, err := http.Get("https://gitlab.com/users/" + username + "/calendar.json")
+
+	if err != nil {
+		log.Fatal("ErrRequest:", err)
+	}
+
+	defer response.Body.Close()
+
+	var body map[string]interface{}
+	bodyResponse, _ := io.ReadAll(response.Body)
+	_ = json.Unmarshal(bodyResponse, &body)
+
+	calendarDates := getCalendarDates()
+	calendarCommitData := make([][]int, len(calendarDates))
+
+	for weekIndex, weekValue := range calendarDates {
+		days := make([]int, len(weekValue))
+
+		for dayIndex, dayValue := range weekValue {
+			if body[dayValue] != nil {
+				days[dayIndex] = int(body[dayValue].(float64))
+			} else {
+				days[dayIndex] = 0
+			}
+		}
+
+		calendarCommitData[weekIndex] = days
+	}
+
+	return calendarCommitData
+}
+
+// Получение от-до дат в двумерном массиве
+func getCalendarDates() [][]string {
+	timeNow := time.Now()
+	timeAfter := timeNow.AddDate(0, -9, 0)
+	timeAfter = timeAfter.AddDate(0, 0, -int(timeAfter.Weekday()+0))
+	timeDiff := int(timeNow.Sub(timeAfter) / (24 * time.Hour))
+
+	var calendarDates []string
+
+	for i := timeDiff; i >= 0; i-- {
+		calendarDates = append(calendarDates, timeNow.AddDate(0, 0, -i).Format("2006-01-02"))
+	}
+
+	return chunkBy(calendarDates, 7)
+}
+
+// Генерация календаря
 func renderCalendar(s *svg.SVG, calendar *Calendar) {
-	for i, week := range calendar.data {
-		for j, day := range week {
-			println(day)                     //TODO: пока в разработке
-			randomNumber := rand.Intn(5) - 1 //TODO: пока в разработке
+	for weekIndex, weekValue := range calendar.data {
+		for dayIndex, dayValue := range weekValue {
+			var style string
 
-			style := ""
-
-			switch randomNumber { //TODO: пока в разработке
-			case 0:
+			switch {
+			case dayValue == 0:
 				style = "fill:#ededed"
-			case 1:
+			case dayValue < 10:
 				style = "fill:rgb(172, 213, 242)"
-			case 2:
+			case dayValue < 20:
 				style = "fill:rgb(127, 168, 201)"
-			case 3:
+			case dayValue < 30:
 				style = "fill:rgb(82, 123, 160)"
-			case 4:
+			default:
 				style = "fill:rgb(37, 78, 119)"
 			}
 
 			s.Square(
-				calendar.margin/2+calendar.squareCalendar.size*i,
-				calendar.margin/2+calendar.squareCalendar.size*j,
+				calendar.margin/2+calendar.squareCalendar.size*weekIndex,
+				calendar.margin/2+calendar.squareCalendar.size*dayIndex,
 				calendar.squareCalendar.size-calendar.squareCalendar.margin,
 				style,
 			)
@@ -81,7 +159,7 @@ func renderCalendar(s *svg.SVG, calendar *Calendar) {
 }
 
 // Разбиение []int на чанки по n размеру чанка
-func chunkBy(items []int, chunkSize int) (chunks [][]int) {
+func chunkBy(items []string, chunkSize int) (chunks [][]string) {
 	for chunkSize < len(items) {
 		items, chunks = items[chunkSize:], append(chunks, items[0:chunkSize:chunkSize])
 	}
